315923fc5ea1ff952564ca2e542a18940744a077
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-16 13:43:13 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 
-def print_fibonacci(length):
+def generate_fibonacci(length):
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+
     initiator = [0, 1]
 
     while len(initiator) < length:
         new_num = initiator[-1] + initiator[-2]
         initiator.append(new_num)
-    print(initiator)
+    return initiator
+
+
+def print_fibonacci(length):
+    fibonacci_sequence = generate_fibonacci(length)
+    print(fibonacci_sequence)
 
 
 print_fibonacci(10)
