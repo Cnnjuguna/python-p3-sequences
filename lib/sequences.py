21e02aa5bf580983db359740c247fa0053780a32
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-16 13:24:24 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    initiator = [0, 1]
+
+    while len(initiator) < length:
+        new_num = initiator[-1] + initiator[-2]
+        initiator.append(new_num)
+    print(initiator)
+
+
+print_fibonacci(10)
